bf49f00c7711ef199e2720551e2946caaa7d3853
JohnMoran <johnnymoran44@hotmail.com>
2021-05-19 17:48:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const port = process.env.PORT || 8080;
 corsProxy
 	.createServer({
 		originWhitelist: ["http://localhost:3000", "https://api.twitter.com/2/tweets/search/recent"],
-		requireHeader: [
-			"origin",
-			"x-requested-with",
-			{
-				Authorization: 'OAuth oauth_consumer_key="yoioBYllFiL5GpGelbK2RDSrm",oauth_token="69931513-ZlF6fnNTlFO1FXvEbR2Z9qhXoSVYfIOGBuwF0v8We",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1621438833",oauth_nonce="pdTCfh4qChb",oauth_version="1.0",oauth_signature="blONvIiYdk5e%2FnXGnrTqsjiMvpA%3D"',
-				Cookie: 'guest_id=v1%3A162142193466870142; personalization_id="v1_8pNsppXnO518XD1sdJB8TQ=="; lang=en',
-			},
-		],
+		requireHeader: ["origin", "x-requested-with"],
 		removeHeaders: ["cookie", "cookie2"],
 	})
 	.listen(port, host, () => {
